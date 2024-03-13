461f331d75e55f981da58d75ac2b103ddcd89140
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-03-13 12:27:40 +0100

    feat: added the html

--- file.js
+++ file.js
@@ -13,6 +13,8 @@ per i numeri multipli di tre e contemporaneamente di 5 stampare la parola "FizzB
 
 
 let n = 100;
+let oi = document.getElementById("num");
+
 
 for (let i = 1; i <= n; i++) {
     let numdicinque = i % 5;
@@ -20,20 +22,26 @@ for (let i = 1; i <= n; i++) {
 
     if (numditre == 0 && numdicinque == 0) {
         console.log("FIZZBUZZ");
+        oi.innerHTML += `<p> "FIZZBUZZ" </p>`;
 
 
     } else if (numdicinque == 0) {
         console.log("Buzz");
+        oi.innerHTML += `<p> BUZZ </p>`;
 
 
     } else if (numditre == 0) {
         console.log("Fizz");
+        oi.innerHTML += `<p> Fizz </p>`;
 
     } else {
         console.log(i);
+        oi.innerHTML += `<p> ${i} </p>`;
 
     }
 }
+
+/*Ho poi inserito tutto nel HTML a breve se riesco provo a sperimentare ancora */
 
 
 
